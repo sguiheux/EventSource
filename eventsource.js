edd1cb647e1d42f0343ef0899298061dff0d202e
Kalugin Sergey <kalugin.perm@gmail.com>
2018-09-03 11:01:55 +0500

    Update eventsource.js

--- eventsource.js
+++ eventsource.js
@@ -470,7 +470,7 @@ var EventSourcePolyfill = (function (global) {
             }, 0);
         }
 
-        onTimeout = funcion() {
+        onTimeout = function() {
             timeout = 0;
             if (currentState !== WAITING) {
                 onEvent("");
